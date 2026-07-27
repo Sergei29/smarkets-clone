9107655e0b11b9f3f00537f592b77de2fe92ab90
Serge Basangovs <sergejs.basangovs@gmail.com>
2026-07-27 16:38:26 +0100

    fix: rm throw err on not implemented routes

--- src/app/api/smarkets/navigation/route.ts
+++ src/app/api/smarkets/navigation/route.ts
@@ -2,6 +2,5 @@ import { NextResponse } from "next/server"
 
 /** proxies GET navigation data */
 export const GET = async (req: Request) => {
-  throw new Error("Not implemented yet")
   return NextResponse.json({ message: "Hello world!" })
 }
--- src/app/api/smarkets/profile/route.ts
+++ src/app/api/smarkets/profile/route.ts
@@ -2,6 +2,5 @@ import { NextResponse } from "next/server"
 
 /** proxies GET /v0/users/current/info-without-rate/ */
 export const GET = async (req: Request) => {
-  throw new Error("Not implemented yet")
   return NextResponse.json({ message: "Hello world!" })
 }
