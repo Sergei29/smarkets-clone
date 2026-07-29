4cbd07ee4e17714b5f3a6f3f207c877a4574a765
Sergei29 <34629784+Sergei29@users.noreply.github.com>
2026-07-29 14:25:56 +0300

    chore: rm redundant code, README.md updated (#17)

--- src/app/api/smarkets/profile/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server"
-
-/** proxies GET /v0/users/current/info-without-rate/ */
-export const GET = async (req: Request) => {
-  return NextResponse.json({ message: "Hello world!" })
-}
